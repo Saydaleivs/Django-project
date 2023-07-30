8b8fa703bc01741c54ff9a0f3ea3a116c2f9d6f7
saydaleivs <saydaleiv@gmail.com>
2023-07-30 16:24:45 +0500

    commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q-^y%=bho7q1%tx5*#dfz#ko63xww@f**63*%0ox3k$8gd0-1d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['render.com']
+ALLOWED_HOSTS = ['django-custom-api.onrender.com']
 
 
 # Application definition
